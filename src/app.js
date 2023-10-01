c7120d964d8e556a71c1efc63e76e8c3f2c4a98c
翁柏豐 <s339428326@gmail.com>
2023-10-01 16:30:13 +0800

    fix:test

--- src/app.js
+++ src/app.js
@@ -11,12 +11,16 @@ dotenv.config({ path: 'config.env' });
 const api = require('./routes/index');
 
 const app = express();
-app.set('view engine', 'ejs');
-app.set('views', `${__dirname}/views`);
+// app.set('view engine', 'ejs');
+// app.set('views', `${__dirname}/views`);
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+app.get('/', (req, res) => {
+  res.send('<h1>123</h1>');
+});
 
 app.use(cors());
 app.use(express.json({ limit: '2mb' })); //req 超過2mb 會停止回應
