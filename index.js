a878e698113b422b00ccf03b5a4d5e231a2ef2ae
翁柏豐 <s339428326@gmail.com>
2023-10-02 00:28:28 +0800

    test: block index

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
+// const dotenv = require('dotenv');
+// const mongoose = require('mongoose');
 
-const app = require('./src/app');
+// const app = require('./src/app');
 
-dotenv.config({ path: 'config.env' });
+// dotenv.config({ path: 'config.env' });
 
-const port = process.env.PORT || 8080;
+// const port = process.env.PORT || 8080;
 
-// const db = mongoose.connection;
+// // const db = mongoose.connection;
 
-// mongoose.connect(process.env.MONGODB_URI, {
-//   useNewUrlParser: true,
-// });
+// // mongoose.connect(process.env.MONGODB_URI, {
+// //   useNewUrlParser: true,
+// // });
 
-// db.on('error', () => console.log('DateBase 連結失敗！'));
-// db.on('open', () => console.log('DateBase 連結成功！'));
+// // db.on('error', () => console.log('DateBase 連結失敗！'));
+// // db.on('open', () => console.log('DateBase 連結成功！'));
 
-const server = app.listen(port, () =>
-  console.log(
-    `NODE_ENV=${
-      process.env.NODE_ENV
-    }\nApplication Start Port:${port}\n前端主機位置抓取：${
-      process.env.NODE_ENV === 'production'
-        ? process.env?.FRONT_END_SERVER
-        : process.env?.FRONT_END_LOCAL
-    }`
-  )
-);
+// const server = app.listen(port, () =>
+//   console.log(
+//     `NODE_ENV=${
+//       process.env.NODE_ENV
+//     }\nApplication Start Port:${port}\n前端主機位置抓取：${
+//       process.env.NODE_ENV === 'production'
+//         ? process.env?.FRONT_END_SERVER
+//         : process.env?.FRONT_END_LOCAL
+//     }`
+//   )
+// );
