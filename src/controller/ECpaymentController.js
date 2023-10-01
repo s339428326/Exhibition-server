0c5c0a76b0e897011821ac254a621e247c454a98
翁柏豐 <s339428326@gmail.com>
2023-10-02 02:35:10 +0800

    fix:ecpyment retuen url

--- src/controller/ECpaymentController.js
+++ src/controller/ECpaymentController.js
@@ -52,7 +52,7 @@ exports.createECOrder = catchAsync(async (req, res, next) => {
     //     : process.env.FRONT_END_SERVER
     // }/#/`,
     // https://evening-hollows-08215-2bb8e1b9252d.herokuapp.com/api/v1/ec/checkMAC
-    ReturnURL: `https://evening-hollows-08215-2bb8e1b9252d.herokuapp.com/api/v1/ec/checkMAC`, //API TO CHECK MAC
+    ReturnURL: `https://exhibition-server.vercel.app/api/v1/ec/checkMAC`, //API TO CHECK MAC
     ClientBackURL: `${
       process.env.NODE_ENV === 'development'
         ? process.env.FRONT_END_LOCAL
