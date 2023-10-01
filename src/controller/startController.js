f31824749a9c5a05776e70d34dcff28322493344
翁柏豐 <s339428326@gmail.com>
2023-10-02 01:19:03 +0800

    fix:heroku

--- src/controller/startController.js
+++ src/controller/startController.js
@@ -4,19 +4,8 @@ const handlerFactory = require('./handlerFactory');
 const Exhibition = require('../model/exhibitionModel');
 
 exports.startUp = catchAsync(async (req, res, next) => {
-  const exhibition = await Exhibition.find({});
-
-  console.log(exhibition);
-
-  if (!exhibition.length)
-    return res.status(404).json({
-      status: 'start',
-      message: 'vercel next middleware fail!',
-    });
-
   //test get env var
   res.status(200).json({
     status: 'start',
-    exhibition,
   });
 });
