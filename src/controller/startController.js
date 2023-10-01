630a938469807e0cc29b637c7d0f9d7e69b9cea7
翁柏豐 <s339428326@gmail.com>
2023-10-01 15:54:30 +0800

    test:vercal

--- src/controller/startController.js
+++ src/controller/startController.js
@@ -3,7 +3,10 @@ const AppError = require('../utils/AppError');
 const handlerFactory = require('./handlerFactory');
 
 exports.startUp = catchAsync(async (req, res, next) => {
+  //test get env var
+  const { DATABASE } = process.env;
   res.status(200).json({
     status: 'start',
+    DATABASE,
   });
 });
