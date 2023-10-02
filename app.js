43aadad9f700e626d1c3b339dc01798663688730
翁柏豐 <s339428326@gmail.com>
2023-10-02 22:22:45 +0800

    fix:vercel

--- app.js
+++ app.js
@@ -81,19 +81,19 @@ app.get('/', (req, res) => {
 app.post('/return', async (req, res) => {
   console.log('req.body:', req.body);
 
-  const { CheckMacValue } = req.body;
-  const data = { ...req.body };
-  delete data.CheckMacValue; // 此段不驗證
+  // const { CheckMacValue } = req.body;
+  // const data = { ...req.body };
+  // delete data.CheckMacValue; // 此段不驗證
 
-  const create = new ecpay_payment(options);
-  const checkValue = create.payment_client.helper.gen_chk_mac_value(data);
+  // const create = new ecpay_payment(options);
+  // const checkValue = create.payment_client.helper.gen_chk_mac_value(data);
 
-  console.log(
-    '確認交易正確性：',
-    CheckMacValue === checkValue,
-    CheckMacValue,
-    checkValue
-  );
+  // console.log(
+  //   '確認交易正確性：',
+  //   CheckMacValue === checkValue,
+  //   CheckMacValue,
+  //   checkValue
+  // );
 
   // 交易成功後，需要回傳 1|OK 給綠界
   res.send('1|OK');
