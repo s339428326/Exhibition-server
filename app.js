d74f4442dc4a053d3f20a35e0a44d2dce0e57ab2
翁柏豐 <s339428326@gmail.com>
2023-10-02 22:42:58 +0800

    test:ec payment reuten empty object issuse

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ dotenv.config({ path: 'config.env' });
 
 const mongoDB = require('./src/mongoDB');
 const api = require('./src/routes/index');
+const view = require('./src/routes/paymentRoute');
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -17,99 +18,13 @@ app.set('views', `${__dirname}/views`);
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
 mongoDB();
-
-//////////////////////////
-const crypto = require('crypto');
-const ecpay_payment = require('ecpay_aio_nodejs');
-const { MERCHANTID, HASHKEY, HASHIV, HOST } = process.env;
-
-// SDK 提供的範例，初始化
-// https://github.com/ECPay/ECPayAIO_Node.js/blob/master/ECPAY_Payment_node_js/conf/config-example.js
-const options = {
-  OperationMode: 'Test', //Test or Production
-  MercProfile: {
-    MerchantID: MERCHANTID,
-    HashKey: HASHKEY,
-    HashIV: HASHIV,
-  },
-  IgnorePayment: [
-    //    "Credit",
-    //    "WebATM",
-    //    "ATM",
-    //    "CVS",
-    //    "BARCODE",
-    //    "AndroidPay"
-  ],
-  IsProjectContractor: false,
-};
-let TradeNo;
-
-app.get('/', (req, res) => {
-  // SDK 提供的範例，參數設定
-  // https://github.com/ECPay/ECPayAIO_Node.js/blob/master/ECPAY_Payment_node_js/conf/config-example.js
-  const MerchantTradeDate = new Date().toLocaleString('zh-TW', {
-    year: 'numeric',
-    month: '2-digit',
-    day: '2-digit',
-    hour: '2-digit',
-    minute: '2-digit',
-    second: '2-digit',
-    hour12: false,
-    timeZone: 'UTC',
-  });
-  TradeNo = 'test' + new Date().getTime();
-  let base_param = {
-    MerchantTradeNo: TradeNo, //請帶20碼uid, ex: f0a0d7e9fae1bb72bc93
-    MerchantTradeDate,
-    TotalAmount: '100',
-    TradeDesc: '測試交易描述',
-    ItemName: '測試商品等',
-    ReturnURL: `${HOST}/return`,
-    ClientBackURL: `${HOST}/clientReturn`,
-  };
-  const create = new ecpay_payment(options);
-
-  // 注意：在此事直接提供 html + js 直接觸發的範例，直接從前端觸發付款行為
-  const html = create.payment_client.aio_check_out_all(base_param);
-  console.log(html);
-
-  res.send(html);
-});
-
-// 後端接收綠界回傳的資料
-app.post('/return', async (req, res) => {
-  console.log('req.body:', req.body);
-
-  // const { CheckMacValue } = req.body;
-  // const data = { ...req.body };
-  // delete data.CheckMacValue; // 此段不驗證
-
-  // const create = new ecpay_payment(options);
-  // const checkValue = create.payment_client.helper.gen_chk_mac_value(data);
-
-  // console.log(
-  //   '確認交易正確性：',
-  //   CheckMacValue === checkValue,
-  //   CheckMacValue,
-  //   checkValue
-  // );
-
-  // 交易成功後，需要回傳 1|OK 給綠界
-  res.send('1|OK');
-});
-
-// 用戶交易完成後的轉址
-app.get('/clientReturn', (req, res) => {
-  console.log('clientReturn:', req.body, req.query);
-  res.send('Done!');
-});
-
-//////////////////////////
 
 app.use(cors());
 app.use(express.json({ limit: '2mb' })); //req 超過2mb 會停止回應
 
+app.use('/', view);
 app.use('/api/v1', api);
 
 //
