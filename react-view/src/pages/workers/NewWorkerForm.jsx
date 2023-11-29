9a6ee0b4ebaf5840bde33ffaa6c403492d29f5ef
翁柏豐 <s339428326@gmail.com>
2023-11-29 16:34:38 +0800

    refactor:add react hook form useForm to parent compoent

--- react-view/src/pages/workers/NewWorkerForm.jsx
+++ react-view/src/pages/workers/NewWorkerForm.jsx
@@ -1,46 +1,91 @@
+import { useState } from 'react';
+import propTypes from 'prop-types';
+import useFetch from '@/hooks/useFetch';
 import Form from '@/components/Form';
 import Input from '@/components/Input';
 import Select from '@/components/Select';
+import { useForm } from 'react-hook-form';
+import { MdOutlineArrowBackIos } from 'react-icons/md';
+//[Feature] add New Worker validation
+import { yupResolver } from '@hookform/resolvers/yup';
 
-const NewWorkerForm = ({ onSubmit }) => {
+const NewWorkerForm = ({ onSubmit, show }) => {
+  const { data, isLoading, fetchError } = useFetch(
+    'get',
+    '/api/v1/department/'
+  );
+
+  const [page, setPage] = useState(0);
+
+  const { handleSubmit, register, formState, watch } = useForm({
+    // resolver: yupResolver(),
+  });
+
+  const watchDepartment = watch('department');
+
   return (
-    <Form className="flex flex-col gap-2" onSubmit={onSubmit}>
-      <Input
-        labelName={'員工姓名'}
-        name={'username'}
-        placeholder="請輸入員工姓名"
-      />
-      <Input
-        labelName={'員工信箱'}
-        name={'email'}
-        placeholder="請輸入員工信箱"
-      />
-      <Select
-        labelName={'職稱'}
-        name={'role'}
-        data={[
-          { name: '請選擇職稱' },
-          { name: '一般員工', value: 'normal' },
-          { name: '管理職', value: 'manger' },
-          { name: '網站管理員', value: 'admin' },
-        ]}
-      />
-      <Select
-        labelName={'部門'}
-        name={'department'}
-        data={[
-          { name: '請選擇部門' },
-          { name: '運營', value: '運營' },
-          { name: '人資', value: '人資' },
-          { name: '客服', value: '客服' },
-          { name: '資訊', value: '資訊' },
-        ]}
-      />
-      <button className="btn w-full" type="submit">
-        新增
-      </button>
-    </Form>
+    <>
+      {page === 1 && (
+        <Form
+          register={register}
+          formState={formState}
+          className="flex flex-col gap-2"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <Input
+            labelName={'員工姓名'}
+            name={'username'}
+            placeholder="請輸入員工姓名"
+          />
+          <Input
+            labelName={'員工信箱'}
+            name={'email'}
+            placeholder="請輸入員工信箱"
+          />
+          <Select
+            labelName={'部門'}
+            name={'department'}
+            data={data?.departmentData?.map((it) => {
+              return { name: it.name, value: it._id };
+            })}
+          />
+          <Select
+            labelName={'職稱'}
+            name={'role'}
+            data={
+              data?.departmentData?.find((it) => it._id === watchDepartment)
+                ?.position
+            }
+          />
+          <button
+            type="submit"
+            onClick={() => setPage((pre) => pre + 1)}
+            className="btn w-full"
+          >
+            下一步
+          </button>
+        </Form>
+      )}
+      {page === 2 && (
+        <div>
+          <button className="btn btn-circle p-4" type="button">
+            <MdOutlineArrowBackIos />
+          </button>
+          <p>申請帳戶詳細資料 + 隱蔽資訊</p>
+          <p>帳戶密碼</p>
+          <p>入職申請PDF 列印</p>
+          <button className="btn" type="button">
+            完成
+          </button>
+        </div>
+      )}
+    </>
   );
 };
 
 export default NewWorkerForm;
+
+NewWorkerForm.propTypes = {
+  show: propTypes.bool,
+  onSubmit: propTypes.func,
+};
