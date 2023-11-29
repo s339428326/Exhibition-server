c21a7e9bf733c9ce479c4d3b6d61f1a707565580
翁柏豐 <s339428326@gmail.com>
2023-11-29 16:20:03 +0800

    doc:remove useForm to component props

--- react-view/src/components/Form/index.jsx
+++ react-view/src/components/Form/index.jsx
@@ -2,27 +2,18 @@
 import { Children, createElement, forwardRef } from 'react';
 import propTypes from 'prop-types';
 
-import { yupResolver } from '@hookform/resolvers/yup';
-import { useForm } from 'react-hook-form';
-
 const Form = forwardRef(
-  ({ defaultValues, schema, children, onSubmit, ...rest }, ref) => {
+  ({ children, onSubmit, register, formState, ...rest }, ref) => {
     //Tips.1 使用React Hook Form 的 useFrom Hook
     //Tips.2 針對useForm 得 解構出 方法
     //Tips.3 使用React預設 props.children 取得 子元件, 並使用React.Children.map 建立createElement
     //Tips.4 React.Children
     //(參數1:當前要遍歷得props.children, 參數2:(參數:1陣列元素, 參數:2陣列位置))
 
-    //resolver 使用 yup 驗證
-    const { handleSubmit, register, formState } = useForm({
-      defaultValues,
-      resolver: schema && yupResolver(schema),
-    });
     //[Dev] 觀看children type => Array
     //useEffect(() => console.log('children', children), []);
-
     return (
-      <form ref={ref} onSubmit={handleSubmit(onSubmit)} {...rest} noValidate>
+      <form ref={ref} onSubmit={onSubmit} {...rest} noValidate>
         {Children.map(children, (child) => {
           return child.props.name
             ? createElement(child.type, {
@@ -43,6 +34,7 @@ Form.propTypes = {
   defaultValues: propTypes.object,
   schema: propTypes.object,
   children: propTypes.node,
+  useForm: propTypes.func,
   type: propTypes.string,
 };
 
