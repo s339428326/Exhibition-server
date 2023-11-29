4b01d7e74026098af18b4a37e9bf89184f44136b
翁柏豐 <s339428326@gmail.com>
2023-11-29 16:32:32 +0800

    Revert "refactor:add react hook form useForm to parent compoent"
    This reverts commit 82f7b621c6d074e2bf358fc7dcb7ea43c6da861f.

--- react-view/src/pages/workers/NewDepartmentForm.jsx
+++ react-view/src/pages/workers/NewDepartmentForm.jsx
@@ -2,60 +2,34 @@ import { useEffect, useState, useRef } from 'react';
 import Form from '@/components/Form';
 import Input from '@/components/Input';
 import useFetch from '@/hooks/useFetch';
-import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import newDepartmentSchema from '@/validation/newDepartmentSchema';
+import newDepartmentSchema from '@/validation/NewDepartmentSchema';
 
-const NewDepartmentForm = ({ setDepartmentList }) => {
+const NewDepartmentForm = () => {
   //[Bug] React Hook over design
-  //[POST] useHook API
   const [postData, setPostData] = useState(null);
-  const [viewErrMsg, setViewErrorMsg] = useState(false);
   const formRef = useRef(null);
-  const { data, isLoading, fetchError } = useFetch(
+  const { data, isLoading } = useFetch(
     'post',
-    '/api/v1/department',
+    '/api/v1/admin/department',
     postData
   );
-  const { handleSubmit, register, formState, watch } = useForm({
-    resolver: yupResolver(newDepartmentSchema),
-  });
-  const resetHandler = () => {
-    setViewErrorMsg(false);
-    formRef.current.reset();
-    document.getElementById('worker-modal').close();
-  };
 
-  //Reset Form value
   useEffect(() => {
     if (data) {
-      resetHandler();
+      formRef.current.reset();
+      document.getElementById('worker-modal').close();
     }
   }, [isLoading]);
 
-  //rerender data
-  useEffect(() => {
-    if (!postData || fetchError) return;
-    if (data?.data) setDepartmentList((pre) => [...pre, data.data]);
-    setPostData(null);
-  }, [data]);
-
-  const onSubmit = async (submitData) => {
+  const onSubmit = (submitData) => {
     setPostData(submitData);
-    if (fetchError) {
-      setViewErrorMsg(true);
-      setTimeout(() => {
-        setViewErrorMsg(false);
-      }, 2500);
-    }
   };
 
   return (
     <Form
       ref={formRef}
-      onSubmit={handleSubmit(onSubmit)}
-      register={register}
-      formState={formState}
+      onSubmit={onSubmit}
+      schema={newDepartmentSchema}
       className="flex flex-col gap-4"
     >
       <Input
@@ -69,7 +43,6 @@ const NewDepartmentForm = ({ setDepartmentList }) => {
         name={'memberCount'}
         placeholder={'請輸入部門人數'}
       />
-      <p className="text-red-500">{viewErrMsg && '請注意部門名稱是否錯誤'}</p>
       <button className="btn ">
         {isLoading ? (
           <span className="loading loading-dots loading-sm"></span>
