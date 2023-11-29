696d84b10825fc1a2cdabdfa47be917c4f1e60b7
翁柏豐 <s339428326@gmail.com>
2023-11-29 17:08:34 +0800

    doc: new department and worker to router index

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,10 +9,11 @@ const startRouter = require('./startRoutes');
 const authRouter = require('./authRoutes');
 const ticketRouter = require('./ticketRoutes');
 const ecRouter = require('./ecRoutes');
-const adminRouter = require('./adminRoutes');
+// const adminRouter = require('./adminRoutes');
 const hostRouter = require('./hostRoutes');
 const workerRouter = require('./workerRoutes');
 const partnerRouter = require('./partnerRoutes');
+const departmentRouter = require('./departmentRoutes');
 
 router.use('/user', userRouter);
 router.use('/auth', authRouter);
@@ -21,9 +22,10 @@ router.use('/order', orderRouter);
 router.use('/ticket', ticketRouter);
 router.use('/ec', ecRouter);
 router.use('/start', startRouter);
-router.use('/admin', adminRouter);
+// router.use('/admin', adminRouter);
 router.use('/host', hostRouter);
 router.use('/worker', workerRouter);
 router.use('/partner', partnerRouter);
+router.use('/department', departmentRouter);
 
 module.exports = router;
