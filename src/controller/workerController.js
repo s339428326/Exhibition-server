778181a2ba49ec3e8340f376b2da2b21b74136aa
翁柏豐 <s339428326@gmail.com>
2023-12-01 00:47:01 +0800

    feat:add new worker controller

--- src/controller/workerController.js
+++ src/controller/workerController.js
@@ -1,6 +1,7 @@
 //公司員工控制
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/AppError');
+const generatePassword = require('../utils/generatePassword');
 const handlerFactory = require('./handlerFactory');
 
 const Worker = require('../model/workerModel');
@@ -16,9 +17,12 @@ exports.createWorker = catchAsync(async (req, res, next) => {
   if (!username || !email || !role || !department)
     return next(new AppError('表單傳送失敗!, 請通知資訊室'));
 
+  const password = generatePassword(8);
+
   //username, email, role, department
   const worker = await Worker.create({
     username,
+    password,
     email,
     role,
     department,
@@ -26,6 +30,6 @@ exports.createWorker = catchAsync(async (req, res, next) => {
 
   res.status(200).json({
     status: 'success',
-    data: worker,
+    data: { worker, password },
   });
 });
